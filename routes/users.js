7d556bd6654a2b397a845b79cfcf4c5ff3231266
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-28 15:21:36 -0400

    full crud established and routes are protected.

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 const User = require('../models/User');
+const isUser = require('../middleware/isUser')
+const isAuthenticated = require("../middleware/isAuthenticated");
+
+// retrieve all users
+router.get("/", (req, res, next) => {
+  User.find()
+    .then((foundUsers) => {
+      console.log("Retrieved all users ====>", foundUsers);
+      res.json(foundUsers);
+    })
+    .catch((err) => {
+      console.log("Error retrieving all users", err);
+      res.json({ errorMessage: "Error retrieving all users", err });
+    });
+});
+
 
 //Get by Id
-router.get('/details/:userId', (req, res, next) => {
+router.get('/details/:userId', isAuthenticated, isUser, (req, res, next) => {
 
   User.findById(req.params.userId)
     .then((foundUser) => {
@@ -17,8 +33,24 @@ router.get('/details/:userId', (req, res, next) => {
     })
 })
 
+// Update by Id
+router.post('/update/:userId', isAuthenticated, isUser, (req, res, next) => {
+
+  User.findByIdAndUpdate(req.params.userId, req.body, {
+    new: true,
+  })
+    .then((updatedUser) => {
+      console.log("Updated user ===>", updatedUser)
+      res.json(updatedUser)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.json(err)
+    })
+})
+
 // Delete by Id
-router.get('/delete/:userId', (req, res, next) => {
+router.get('/delete/:userId', isAuthenticated, isUser, (req, res, next) => {
 
   User.findByIdAndDelete(req.params.userId)
     .then((deletedUser) => {
