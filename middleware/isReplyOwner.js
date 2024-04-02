6ba0842774c678138c1afbd59d969ebd386961bc
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-04-02 19:16:40 -0400

    created middleware for reply owner validation

---
+++ middleware/isReplyOwner.js
@@ -0,0 +1,18 @@
+const Reply = require('../models/Reply')
+const isReplyOwner = (req, res, next) => {
+    const { replyId } = req.params;
+    Comment.findById(foundReply)
+        .then((foundReply) => {
+            if (foundReply.owner.toString() === req.user._id) {
+                next()
+            } else {
+                res.status(402).json({message: "Not authorized."})
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json(err)
+        })
+}
+
+module.exports = isReplyOwner
