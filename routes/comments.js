7a7c40e2516949c6daaa7e82b6033e859fa51a78
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-04-02 15:48:07 -0400

    fixed route to push into bill

--- routes/comments.js
+++ routes/comments.js
@@ -2,13 +2,14 @@ var express = require("express");
 var router = express.Router();
 
 const Comment = require("../models/Comment");
+const Bill = require("../models/Bill");
 const isAuthenticated = require("../middleware/isAuthenticated");
-const isCommentOwner = require('../middleware/isCommentOwner');
+const isCommentOwner = require("../middleware/isCommentOwner");
 
 // retrieve all comments
 router.get("/", (req, res, next) => {
   Comment.find()
-    .populate('owner')
+    .populate("owner")
     .then((foundComment) => {
       console.log("Retrieved all comments ====>", foundComment);
       res.json(foundComment);
@@ -21,13 +22,18 @@ router.get("/", (req, res, next) => {
 
 /* new comment */
 router.post("/new", isAuthenticated, (req, res, next) => {
-  const { comment, upVotes, downVotes} = req.body;
-  
+  const { comment, upVotes, downVotes, billId } = req.body;
 
-  Comment.create({ comment, upVotes, downVotes, owner:req.user._id })
+  Comment.create({ comment, owner: req.user._id, bill: billId })
     .then((createdComment) => {
       console.log("Created a new comment ====>", createdComment);
-      res.json(createdComment);
+      return Bill.findByIdAndUpdate(
+        createdComment.bill,
+        {
+          $push: { comments: createdComment._id },
+        },
+        { new: true }
+      );
     })
     .then((response) => res.json(response))
     .catch((err) => {
@@ -37,32 +43,42 @@ router.post("/new", isAuthenticated, (req, res, next) => {
 });
 
 // update commment by id
-router.post("/update/:commentId", isAuthenticated, isCommentOwner, (req, res, next) => {
-  Comment.findByIdAndUpdate(req.params.commentId, req.body, {
-    new: true,
-  })
-    .then((updatedComment) => {
-      console.log("Updated comment ====>", updatedComment);
-      res.json(updatedComment);
+router.post(
+  "/update/:commentId",
+  isAuthenticated,
+  isCommentOwner,
+  (req, res, next) => {
+    Comment.findByIdAndUpdate(req.params.commentId, req.body, {
+      new: true,
     })
-    .catch((err) => {
-      console.log("Error updating comment", err);
-      res.json({ errorMessage: "Error updating specified comment", err });
-    });
-});
+      .then((updatedComment) => {
+        console.log("Updated comment ====>", updatedComment);
+        res.json(updatedComment);
+      })
+      .catch((err) => {
+        console.log("Error updating comment", err);
+        res.json({ errorMessage: "Error updating specified comment", err });
+      });
+  }
+);
 
 // delete comment by id
 
-router.delete("/delete/:commentId", isAuthenticated, isCommentOwner, (req, res, next) => {
-  Comment.findByIdAndDelete(req.params.commentId)
-    .then((deletedComment) => {
-      console.log("Deleted ===>", deletedComment);
-      res.json(deletedComment);
-    })
-    .catch((err) => {
-      console.log("Error deleting comment ====>", err);
-      res.status(502).json(err);
-    });
-});
+router.delete(
+  "/delete/:commentId",
+  isAuthenticated,
+  isCommentOwner,
+  (req, res, next) => {
+    Comment.findByIdAndDelete(req.params.commentId)
+      .then((deletedComment) => {
+        console.log("Deleted ===>", deletedComment);
+        res.json(deletedComment);
+      })
+      .catch((err) => {
+        console.log("Error deleting comment ====>", err);
+        res.status(502).json(err);
+      });
+  }
+);
 
 module.exports = router;
